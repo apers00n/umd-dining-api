bb12f26e6d060c1da20f60b126a975c1d703afc9
apers00n <natanameyer@gmail.com>
2025-10-17 02:24:43 -0400

    finished basic form

---
+++ components/svgRects.tsx
@@ -0,0 +1,430 @@
+const stroke = "none";
+
+export const Treats = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="76.169"
+      y="158.84"
+      width="52.054"
+      height="35.297"
+      rx="1.04"
+      ry="1.04"
+      stroke={stroke}
+      fill="none"
+      pointerEvents="all"
+      strokeWidth="0.965"
+      strokeDasharray="0.0964999,0.0964999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Treats"])}
+    />
+  );
+};
+export const BreakfastBar = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="96.52"
+      y="122.78"
+      width="52.054"
+      height="35.297"
+      rx="1.04"
+      ry="1.04"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.965"
+      strokeDasharray="0.0964999,0.0964999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Breakfast Bar"])}
+    />
+  );
+};
+export const PeanutButter = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="96.52"
+      y="87.482"
+      width="52.054"
+      height="35.297"
+      rx="1.04"
+      ry="1.04"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.965"
+      strokeDasharray="0.0964999,0.0964999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Peanut Butter"])}
+    />
+  );
+};
+export const PurpleZone = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="190.83"
+      y="56.175"
+      width="52.054"
+      height="35.297"
+      rx="1.04"
+      ry="1.04"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.965"
+      strokeDasharray="0.0964999,0.0964999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Purple Zone", "Purple Zone Sides"])}
+    />
+  );
+};
+export const Pizza = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="190.83"
+      y="91.472"
+      width="52.054"
+      height="35.297"
+      rx="1.04"
+      ry="1.04"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.965"
+      strokeDasharray="0.0964999,0.0964999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Pizza"])}
+    />
+  );
+};
+export const RomaVeganSaladsAndPanini = ({
+  setSelected,
+}: {
+  setSelected: any;
+}) => {
+  return (
+    <rect
+      x="277.4"
+      y="97.969"
+      width="75.262"
+      height="60.528"
+      rx="1.5037"
+      ry="1.7834"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="1.5195"
+      strokeDasharray="0.151951,0.151951"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() =>
+        setSelected(["Roma Vegan Salads and Panini", "Roma Vegan Sides"])
+      }
+    />
+  );
+};
+export const Beverages = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="332.22"
+      y="137.64"
+      width="52.19"
+      height="21.841"
+      rx="1.0427"
+      ry=".64354"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.76009"
+      strokeDasharray="0.0760087,0.0760087"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Beverages"])}
+    />
+  );
+};
+export const MongolianGrill = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="277.4"
+      y="6.4428"
+      width="75.262"
+      height="91.526"
+      rx="1.5037"
+      ry="2.6968"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="1.8685"
+      strokeDasharray="0.186852,0.186852"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() =>
+        setSelected(["Mongolian Grill", "Mongolian Grill Made to Ord"])
+      }
+    />
+  );
+};
+export const Pasta = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="190.83"
+      y="126.77"
+      width="52.054"
+      height="35.297"
+      rx="1.04"
+      ry="1.04"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.965"
+      strokeDasharray="0.0964999,0.0964999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Pasta"])}
+    />
+  );
+};
+export const GrillWorks = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="190.78"
+      y="197.31"
+      width="52.054"
+      height="35.297"
+      rx="1.04"
+      ry="1.04"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.965"
+      strokeDasharray="0.0964999,0.0964999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Grill Works", "Grill Works Sides"])}
+    />
+  );
+};
+export const ChefsTable = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="190.75"
+      y="232.57"
+      width="52.118"
+      height="30.726"
+      rx="1.0413"
+      ry=".90533"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.9009"
+      strokeDasharray="0.0900905,0.0900905"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Chef's Table", "Chef's Table Sides"])}
+    />
+  );
+};
+export const Roaster = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="190.79"
+      y="263.34"
+      width="52.03"
+      height="37.056"
+      rx="1.0395"
+      ry="1.0918"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.98853"
+      strokeDasharray="0.098853,0.098853"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Roaster", "Roaster Sides"])}
+    />
+  );
+};
+export const SoftServeandIceCream = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="190.69"
+      y="300.3"
+      width="60.059"
+      height="21.197"
+      rx="1.2"
+      ry=".62457"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.80327"
+      strokeDasharray="0.0803272,0.0803272"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Treats"])}
+    />
+  );
+};
+export const Desserts = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="215.24"
+      y="321.44"
+      width="35.573"
+      height="26.303"
+      rx=".71074"
+      ry=".77503"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.68866"
+      strokeDasharray="0.0688658,0.0688658"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Desserts", "Vegan Desserts"])}
+    />
+  );
+};
+export const BroilerWorks = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="178.05"
+      y="162.12"
+      width="64.779"
+      height="35.187"
+      rx="1.2943"
+      ry="1.0368"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="1.0748"
+      strokeDasharray="0.107483,0.107483"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Broiler Works", "Broiler Works Sides"])}
+    />
+  );
+};
+export const DeliPlus = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="76.169"
+      y="194.14"
+      width="52.054"
+      height="35.297"
+      rx="1.04"
+      ry="1.04"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.965"
+      strokeDasharray="0.0964999,0.0964999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Deli+"])}
+    />
+  );
+};
+export const Deli = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="76.15"
+      y="229.41"
+      width="52.092"
+      height="32.483"
+      rx="1.0408"
+      ry=".95711"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.92609"
+      strokeDasharray="0.0926088,0.0926088"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Deli"])}
+    />
+  );
+};
+export const Waffles = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="146.62"
+      y="191.32"
+      width="30.59"
+      height="29.876"
+      rx=".61117"
+      ry=".88031"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.68059"
+      strokeDasharray="0.0680592,0.0680592"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Waffle, Doughnut, Bagel Bar"])}
+    />
+  );
+};
+export const Condiments = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="258.54"
+      y="296.85"
+      width="48.866"
+      height="29.699"
+      rx=".97633"
+      ry=".87509"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.85766"
+      strokeDasharray="0.0857659,0.0857659"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Condiments"])}
+    />
+  );
+};
+export const InfusedWaterStation = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="347.86"
+      y="317.12"
+      width="64.122"
+      height="29.577"
+      rx="1.2811"
+      ry=".87147"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="0.98042"
+      strokeDasharray="0.0980422,0.0980422"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Infused Water Station"])}
+    />
+  );
+};
+export const SaladBar = ({ setSelected }: { setSelected: any }) => {
+  return (
+    <rect
+      x="146.66"
+      y="242.35"
+      width="30.152"
+      height="81.849"
+      rx=".60242"
+      ry="2.4117"
+      fill="none"
+      pointerEvents="all"
+      stroke={stroke}
+      strokeWidth="1.1184"
+      strokeDasharray="0.11184,0.11184"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      onClick={() => setSelected(["Salad Bar"])}
+    />
+  );
+};
